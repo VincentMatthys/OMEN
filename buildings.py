99d74743d40443278c75c7b9e0b19dddf1f2f435
Vincent Matthys <vmatthys@student.42.fr>
2017-05-30 15:16:53 +0200

    Getting buildings from a map area

---
+++ buildings.py
@@ -0,0 +1,51 @@
+import overpass
+
+api = overpass.API()
+
+
+def get_buildings(response):
+	"""
+	Returns the list of buildings of the overpass api response
+	"""
+
+	# List of buildings
+	buildings = []
+
+	# For every element in the features of the reponse
+	for element in response['features']:
+		# when the feature is a building
+		if 'building' in element['properties']:
+			buildings.append(element)
+	return buildings
+
+def get_positions(features):
+	"""
+	Given a list of features, return the list of positions
+	"""
+
+	# List of positions
+	positions = []
+
+	# For every element in the features
+	for element in features:
+		# Append the list of coordinates
+		positions.append(element['geometry']['coordinates'])
+
+	return positions
+
+"""
+# Query by name
+response = api.Get('node["name"="Salt Lake City"]')
+
+print ([(feature['properties']['name'], feature['id']) for feature in response['features']])
+
+"""
+
+# Query by map
+map_query = overpass.MapQuery(50.746,7.154,50.748,7.157)
+# Get the response from the API
+response = api.Get(map_query)
+# Get every buildings from this reponse
+buildings = get_buildings(response)
+# Get the positions of every buildings
+pos = get_positions(buildings)
