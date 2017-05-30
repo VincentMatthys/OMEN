81114847078d2b3b3e9cb4045e3192297620e2b2
Vincent Matthys <vmatthys@student.42.fr>
2017-05-30 22:41:17 +0200

    Visualisation OK. WARNING : repartition des tools

--- map.py
+++ map.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from buildings import *
 import matplotlib.pyplot as plt
 import numpy as np
 import overpass
+from tools import convert_metter_to_longitude
+
+import sys
 
 def plot_buildings(positions):
 	"""
@@ -13,9 +15,34 @@ def plot_buildings(positions):
 	for b in positions:
 		temp = np.array(b)
 		plt.plot(temp[:, 0], temp[:, 1], c = 'b')
-	plt.show
 
-def plot_antennas()
+def convert_metter_to_longitude(distance, lattitude):
+	"""
+	Given a distance in metter, and the lattitude, returns the corresponding deltalongitude
+	"""
+	return 360 * distance / (40075160 * math.cos(math.pi * lattitude / 180))
+
+def plot_map(site):
+	"""
+	Given a site stored in the sites dictionary, plot the buildings, then the antenna and the radius
+	"""
+
+	plt.figure()
+
+	plot_buildings(get_positions(site['buildings']))
+
+	# Position of the antenna
+	x_a, y_a = site['LON'], site['LAT']
+
+	# Perimeter of the antenna_field
+	per = site['Antennes'][0]['Perimeter']
+
+	antenna_field = plt.Circle((x_a, y_a), convert_metter_to_longitude(per, y_a), color = 'r')
+
+	plt.gcf().gca().add_artist(antenna_field)
+	plt.show()
+
+#def plot_antennas()
 
 if __name__ == '__main__':
 	api = overpass.API()
@@ -29,5 +56,3 @@ if __name__ == '__main__':
 	pos = get_positions(buildings)
 
 	#test = np.array(pos[0])
-
-	plt.figure()
--- tools.py
+++ tools.py
@@ -2,6 +2,79 @@
 
 import pandas
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+from buildings import *
+from map import *
+import overpass
+
+def get_buildings(response):
+	"""
+	Returns the list of buildings of the overpass api response
+	"""
+
+	# List of buildings
+	buildings = []
+
+	# For every element in the features of the reponse
+	for element in response['features']:
+		# when the feature is a building
+		if 'building' in element['properties']:
+			buildings.append(element)
+	return buildings
+
+def get_positions(features):
+	"""
+	Given a list of features, return the list of positions
+	"""
+
+	# List of positions
+	positions = []
+
+	# For every element in the features
+	for element in features:
+		# Append the list of coordinates
+		positions.append(element['geometry']['coordinates'])
+
+	return positions
+	
+def plot_buildings(positions):
+	"""
+	Given the list of positions of buildings (polygons), plot them
+	"""
+
+	for b in positions:
+		temp = np.array(b)
+		plt.plot(temp[:, 0], temp[:, 1], c = 'b')
+
+def convert_metter_to_longitude(distance, lattitude):
+	"""
+	Given a distance in metter, and the lattitude, returns the corresponding deltalongitude
+	"""
+	return 360 * distance / (40075160 * math.cos(math.pi * lattitude / 180))
+
+def plot_map(site):
+	"""
+	Given a site stored in the sites dictionary, plot the buildings, then the antenna and the radius
+	"""
+
+	plt.figure()
+
+	plot_buildings(get_positions(site['buildings']))
+
+	# Position of the antenna
+	x_a, y_a = site['LON'], site['LAT']
+
+	# Perimeter of the antenna_field
+	per = site['Antennes'][0]['Perimeter']
+
+	antenna_field = plt.Circle((x_a, y_a), convert_metter_to_longitude(per, y_a), color = 'r')
+
+	plt.gcf().gca().add_artist(antenna_field)
+	plt.show()
+
+
 
 def get_features(prefix = "data/", coord_sites_file = "Coordinates.csv", suffix = "_Antennes.csvr"):
 	"""
@@ -70,7 +143,9 @@ def area_per_site(site, factor = 2):
 	Given a site as returned by the get_features function, find the maximal perimeter of the antennas.
 	Factore is the multiplicatif constant applied to the window defined by the perimeter
 	With this max, define an area to call the Overpass API with the call_api_mapQuery function
-	Returns xmin, xmax (LON), ymin, ymax (LAT)
+	Returns the format asked to call api by MapQuery method :
+	south, west, north, east :
+	ymin, xmin, ymax, xmax
 	"""
 
 	# Position of the site
@@ -91,14 +166,28 @@ def area_per_site(site, factor = 2):
 	# Size of area around the site, in lattitude
 	delta_y = factor * convert_metter_to_lattitude(perimeter)
 
-	return x - delta_x, x + delta_x, y - delta_y, y + delta_y
+	return y - delta_y, x - delta_x, y + delta_y, x + delta_x
 
 
-def call_api_mapQuery(xmin, xmax, ymin, ymax):
+def call_api_mapQuery(site, api):
 	"""
-	Give the coordinates of the area where the site
+	Give the buildings of an area as defined by the area_per_site function
+	Entry : take the sites dictionary
+	Returns the list of buildings found on the api
 	"""
 
+	south, west, north, east = area_per_site(sites[site])
+
+	# Query by map
+	map_query = overpass.MapQuery(south, west, north, east)
+
+	# Get the response from the API
+	response = api.Get(map_query)
+
+	# Get every buildings from this reponse
+	buildings = get_buildings(response)
+
+	return buildings
 
 if __name__ == '__main__':
 	# Prefix for data storage
@@ -110,7 +199,11 @@ if __name__ == '__main__':
 	# Extension suffix for site antenna
 	suffix = "_Antennes.csv"
 
+
 	sites = get_features(prefix, coord_sites_file, suffix)
 
+	api = overpass.API()
 	for site in sites:
-		sites[site]['area'] = area_per_site(sites[site])
+		sites[site]['buildings'] = call_api_mapQuery(site, api)
+
+	plot_map(sites['GE_0002A'])
