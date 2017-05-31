94df091d5238ab8c39340db6bee8efcd45d9f317
Vincent Matthys <vmatthys@student.42.fr>
2017-05-31 12:01:20 +0200

    Discretize done

--- tools.py
+++ tools.py
@@ -5,6 +5,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 #from buildings import *
 #from map import *
 import overpass
@@ -57,9 +58,9 @@ def convert_metter_to_longitude(distance, lattitude):
 	"""
 	return 360 * distance / (40075160 * math.cos(math.pi * lattitude / 180))
 
-def plot_map(site):
+def plot_map(dic, sitename):
 	"""
-	Given a site stored in the sites dictionary, plot the buildings, then the antenna and the radius
+	Given a dic and a site name stored in the sites dictionary, plot the buildings, then the antenna and the radius
 	"""
 
 	plt.figure()
@@ -193,32 +194,111 @@ def call_api_mapQuery(site, api, sites):
 	return buildings
 
 def map_to_antenna(sites, site_name):
+    """
+    Given the sites dictionary and a site_name, returns a dictionary as following :
+        - Position of the site (LON, LAT)
+        - Relative positions of the antennas
+        - Relative positions of the buildings
+    """
+
+    relative = {}
+
+    # Position of the site
+    x_site = sites[site_name]['LON']
+    y_site = sites[site_name]['LAT']
+    relative[site_name] = {'LAT' : y_site, 'LON' : x_site}
+
+    # Relative position of the antemma
+    relative['Antennes'] = sites[site_name]['Antennes']
+
+    # Relative positions of the buildings
+    pos_buildings = get_positions(sites[site_name]['buildings'])
+
+    for building in pos_buildings:
+        pos_buildings[building] -= (x_site, y_site)
+        pos_buildings[building] = pos_buildings[building] * (40075160 / 360)
+
+    relative['buildings'] = pos_buildings
+
+    return relative
+
+def extract_radiation_value(fichier):
+    """
+        Extract the radiation value from the radiation pattern input file
+        The file has to contain :
+        - Introduction part with some technical content
+        - First part :
+            -- HORIZONTAL + Number of Discretisation (first line)
+            -- Angle + Value (for other lines)
+        - Second part :
+            -- HORIZONTAL + Number of Discretisation (first line)
+            -- Angle + Value (for other lines)
+        """
+    # Ouvterdure du fichier en lecture seulemenbt
+    f = open(fichier, 'r')
+
+    # Lecture de la première ligne du fichier
+    ligne = f.readline()
+
+    # Initialisation de la liste des valeurs :
+    # 	- values[0] : horizontal attenuation - dictionary
+    #	- values[1] : vertical attenuation - dictionary
+    values = [{}, {}]
+
+    # cur vaut 0 si horizontal part, 1 si vertical, -1 si partie introductive
+    cur = -1
+
+    # Variable de stockage de la discrétisation
+    size = 0
+
+    # Boucle sur toutes les lignes non vides de ligne
+    while ligne != "":
+        # Splited ligne
+        split = ligne.split()
+        # Si la ligne marque le début de la partie horizontal :
+        if split[0] == 'HORIZONTAL' or split[0] == 'VERTICAL':
+            if split[0] == 'HORIZONTAL':
+                cur = 0
+            else:
+                cur = 0
+            size = int(split[1])
+            ligne = f.readline()
+            split = ligne.split()
+
+        # If we are in the first or second part, add values to current dictionary
+        if cur >= 0:
+            values[cur][float(split[0])] = float(split[1])
+        # Go to the next ligne
+        ligne = f.readline()
+    return values[0], values[1]
+
+def discretize(bat, step):
 	"""
-	Given the sites dictionary and a site_name, returns a dictionary as following :
-		- Position of the site (LON, LAT)
-		- Relative positions of the antennas
-		- Relative positions of the buildings
+	Given a numpy array of the bat represented by a polygon
+	Returns a numpy array of coordinates in facade
 	"""
 
-	relative = {}
-   
-	# Position of the site
-	x_site = sites[site_name]['LON']
-	y_site = sites[site_name]['LAT']
-	relative[site_name] = {'LAT' : y_site, 'LON' : x_site}
+	# List of pnew positions
+	p = []
 
-	# Relative position of the antemma
-	relative['Antennes'] = sites[site_name]['Antennes']
+	p.append(bat[0])
 
-	# Relative positions of the buildings
-	pos_buildings = get_positions(sites[site_name]['buildings'])
+	for k in range(1, len(bat)):
+		if k == 0:
+			p.append(bat[k])
+		else :
+			p2 = bat[k]
+			p1 = bat[k - 1]
+			for i in range(step):
+				p.append(p1 + (p2 - p1) * i/step)
 
-	for building in pos_buildings:
-		pos_buildings[building] -= (x_site, y_site)
+	p.append(bat[0])
+	return np.array(p)
 
-	relative['buildings'] = pos_buildings
-
-	return relative
+def plot_bat(bat):
+	plt.figure(figsize = (20, 15))
+	plt.plot(bat[:, 0], bat[:, 1], '-o')
+	plt.show()
 
 ################################################################################
 ############################### MAIN ###########################################
